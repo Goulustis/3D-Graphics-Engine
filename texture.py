24532c0292f2fcb57796d6821577f5b29d9eee76
goulustis <cnimaiv@gmail.com>
2023-05-20 02:18:34 -0700

    add black white box texture

--- texture.py
+++ texture.py
@@ -11,7 +11,12 @@ class Texture:
         self.textures[0] = self.get_texture(path='textures/img.png')
         self.textures[1] = self.get_texture(path='textures/img_1.png')
         self.textures[2] = self.get_texture(path='textures/img_2.png')
+
+        self.textures["white"] = self.get_texture(path="textures/white.png")
+        self.textures["black"] = self.get_texture(path="textures/black.png")
+
         self.textures['cat'] = self.get_texture(path='objects/cat/20430_cat_diff_v1.jpg')
+        self.textures["robo"] = self.get_texture(path="objects/robo/robo_tex.jpg")
         self.textures['skybox'] = self.get_texture_cube(dir_path='textures/skybox1/', ext='png')
         self.textures['depth_texture'] = self.get_depth_texture()
 
