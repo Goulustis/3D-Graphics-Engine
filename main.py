f73feb558715a3ea79d2368fe22d78739e8d4588
goulustis <cnimaiv@gmail.com>
2023-05-21 15:23:03 -0700

    add low memory slow simulation

--- main.py
+++ main.py
@@ -41,8 +41,8 @@ class GraphicsEngine:
         # light
         self.light = Light(position=(-3,2,2))
         # camera
-        self.camera = Camera(self)
-        # self.camera = PlayCamera(self)
+        # self.camera = Camera(self)
+        self.camera = PlayCamera(self)
         # mesh
         self.mesh = Mesh(self)
         # scene
@@ -78,15 +78,16 @@ class GraphicsEngine:
             self.check_events()
             self.camera.update()
             self.render()
-            self.delta_time = self.clock.tick(60)
+            self.delta_time = self.clock.tick(120)
 
 
 class SimulatorEngine:
-    def __init__(self, win_size=(1600, 900), save_frame_dir = "dev_frames"):
+    def __init__(self, win_size=(1600, 900), save_frame_dir = "dev_frames", save_mem=True):
         # init pygame modules
         pg.init()
         # window size
         self.WIN_SIZE = win_size
+        self.save_winsize = (win_size[0]//3, win_size[1]//3)
 
 
         # set opengl attr
@@ -121,9 +122,13 @@ class SimulatorEngine:
 
         # extra attributes:
         self.attr = {"done":False}
-
+        self.save_mem = save_mem
         self.frames = []
         self.save_frame_dir = save_frame_dir
+        if osp.exists(self.save_frame_dir):
+            shutil.rmtree(self.save_frame_dir)
+
+        os.makedirs(self.save_frame_dir, exist_ok=True)
 
     def render(self):
         # clear framebuffer
@@ -136,35 +141,41 @@ class SimulatorEngine:
 
     def run(self):
         print("simulating")
+        curr_idx = 0
         while not (self.attr["done"]):
             self.camera.update()
             self.render()
             img = self.get_img()
-            self.frames.append(img)
 
-        self.save_frames()
+            if self.save_mem:
+                path = osp.join(self.save_frame_dir, str(curr_idx).zfill(6) + ".png")
+                self.save_img(img, path)
+                curr_idx += 1
+            else:
+                self.frames.append(img)
+
+        if not self.save_mem:
+            self.save_all_imgs()
     
-    def save_frames(self, targ_dir=None):
+    def save_img(self, frame:np.ndarray, path:str):
+        img = Image.fromarray(frame).convert('L')
+        img = img.resize(self.save_winsize, resample=Image.LANCZOS)
+        img.save(path)
+    
+    def save_all_imgs(self, targ_dir=None):
         if targ_dir is None:
             targ_dir = self.save_frame_dir
         
-        if osp.exists(targ_dir):
-            shutil.rmtree(targ_dir)
-
-        os.makedirs(targ_dir, exist_ok=True)
-        new_size = (self.WIN_SIZE[0]//2 , self.WIN_SIZE[1]//2)
         for i, img in tqdm(enumerate(self.frames), total=len(self.frames), desc="saving frames"):
             save_path = osp.join(targ_dir, str(i).zfill(6) + ".png")
-            img = Image.fromarray(img).convert('L')
-            img = img.resize(new_size, resample=Image.LANCZOS)
-            img.save(save_path)
+            self.save_img(img, save_path)
             
 
 
     def get_img(self):
         image_buffer = glReadPixels(0, 0, self.WIN_SIZE[0], self.WIN_SIZE[1], OpenGL.GL.GL_RGB, OpenGL.GL.GL_UNSIGNED_BYTE)
         image = np.frombuffer(image_buffer, dtype=np.uint8).reshape(self.WIN_SIZE[1], self.WIN_SIZE[0], 3)
-        return image[::-1]
+        return image[::-1,::-1]
 
 
 
@@ -172,7 +183,7 @@ if __name__ == '__main__':
     fx, fy, cx, cy = read_intrinsics(intrinsics_path)
     win_size = (int(cx*2), int(cy*2))
     # app = GraphicsEngine(win_size=win_size)
-    app = SimulatorEngine(win_size=win_size, save_frame_dir="generated_imgs/cat_light_2048")
+    app = SimulatorEngine(win_size=win_size, save_frame_dir="generated_imgs/cat_lerp")
     app.run()
 
 
