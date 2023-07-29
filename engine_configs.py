91764ac05e0aca291f2a03e40fb751c1d6626403
goulustis <cnimaiv@gmail.com>
2023-07-28 18:38:53 -0700

    add engine configs

---
+++ engine_configs.py
@@ -0,0 +1,5 @@
+### contains global variable for setting up which scene to run
+SCENE = "robo" # one of ["robo", "carpet"]
+MODE = "run"   # one of ["run", "render"]
+               #   - "run" for developement configs
+               #   - "render" for generate configs
